2a8b454c315ed386bbe561e860615a52d27cbab5
amish1188 <martin.joodd@gmail.com>
2020-03-17 16:56:30 +0100

    change code into react

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-        /*
-            The code from bundle.js will be executed when HTML loads it.
-            But, by default, we cannot access it. To be able to access it from
-            other JS in the page, we need to export it as a module.
-            In this case, the module will be called 'EntryPoint', which is a 'var'.
-            Note: this is just an example. In general, we will not need to do something
-            like this, as all the code we will run is from bundle.js itself
-         */
-        libraryTarget: 'var',
-        library: 'EntryPoint'
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
     },
     devServer: {
         contentBase: './public'
